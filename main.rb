6247539b273bf85ee185dc0bd0036408ac76ea0b
atsushi0919 <hatake0919@yahoo.co.jp>
2021-04-20 10:59:25 +0900

    init

---
+++ main.rb
@@ -0,0 +1,34 @@
+puts <<EOS
+\033[2J\033[0;0H
+\e[46m　　　　　　　　　　　　　　　　　　　　　　　　　　　\e[0m
+\e[46m　　　　　　　　　\e[40m　　　　　　　　　\e[46m　　　　　　　　　\e[0m
+\e[46m　　　　　　　　\e[40m　　　　　　　　　　　\e[46m　　\e[40m　\e[46m　　　　　\e[0m
+\e[46m　　　　　　　\e[40m　　\e[45m　　\e[40m　\e[45m　　　　\e[40m　　　　　\e[45m　\e[40m　\e[46m　　　　\e[0m
+\e[46m　　　　　　\e[40m　　\e[45m　\e[40m　　\e[45m　\e[40m　　　　\e[45m　\e[40m　　\e[45m　　\e[40m　\e[46m　　　　　\e[0m
+\e[46m　　　　　　\e[40m　　　　　\e[45m　\e[40m　　　　　　　\e[45m　　　\e[40m　\e[46m　　　　\e[0m
+\e[46m　　　　　\e[40m　　　　　\e[47m　\e[40m　\e[47m　　\e[40m　　　　　　　　\e[46m　　　　　\e[0m
+\e[46m　　　　　\e[40m　　　　\e[47m　　　　　　　　　\e[40m　　　　\e[46m　　　　　\e[0m
+\e[46m　　　　\e[40m　　　　\e[47m　　　　　　　　　　　\e[40m　　　　\e[46m　　　　\e[0m
+\e[46m　　　　\e[40m　　　\e[47m　　\e[40m　　　\e[47m　　　\e[40m　　　\e[47m　　\e[40m　　　\e[46m　　　　\e[0m
+\e[46m　　　　\e[40m　　　\e[47m　\e[40m　\e[47m　　　　　　　　　\e[40m　\e[47m　\e[40m　　　\e[46m　　　　\e[0m
+\e[46m　　　　\e[40m　　　\e[47m　　\e[40m　　　\e[47m　　　\e[40m　　　\e[47m　　\e[40m　　　\e[46m　　　　\e[0m
+\e[46m　　　　\e[40m　　　\e[47m　\e[40m　\e[47m　\e[41m　\e[45m　\e[47m　　　\e[45m　\e[41m　\e[47m　\e[40m　\e[47m　\e[40m　　　\e[46m　　　　\e[0m
+\e[46m　　　　\e[40m　\e[45m　\e[40m　\e[47m　　　\e[45m　　\e[47m　　　\e[45m　　\e[47m　　\e[40m　　\e[45m　\e[40m　\e[46m　　　　\e[0m
+\e[46m　　　　\e[40m　\e[45m　\e[40m　\e[41m　\e[47m　　　　　　　　　　　\e[41m　\e[40m　\e[45m　\e[40m　\e[46m　　　　\e[0m
+\e[46m　　　　\e[40m　　\e[45m　\e[40m　\e[41m　\e[42m　　\e[43m　\e[42m　　　\e[43m　\e[42m　　\e[41m　\e[40m　\e[45m　\e[40m　　\e[46m　　　　\e[0m
+\e[46m　　　\e[40m　　　\e[45m　\e[40m　　\e[42m　　\e[43m　\e[42m　　　\e[43m　\e[42m　　\e[40m　\e[45m　\e[40m　\e[45m　\e[40m　\e[46m　　　　\e[0m
+\e[46m　　\e[40m　　\e[41m　\e[40m　　　　\e[45m　\e[40m　　\e[45m　\e[47m　\e[45m　\e[40m　\e[45m　\e[40m　　\e[45m　\e[40m　\e[45m　　\e[40m　\e[46m　　　\e[0m
+\e[46m　　\e[40m　\e[41m　\e[40m　　\e[41m　\e[40m　\e[45m　\e[40m　　　\e[45m　　　\e[40m　　\e[45m　\e[40m　\e[41m　　\e[45m　\e[41m　\e[40m　　\e[46m　　\e[0m
+\e[46m　\e[40m　\e[41m　　\e[40m　\e[41m　\e[40m　\e[41m　\e[40m　　　\e[41m　　\e[47m　　\e[41m　\e[40m　\e[45m　\e[40m　　\e[41m　　\e[40m　　\e[41m　\e[40m　\e[46m　\e[0m
+\e[46m　\e[40m　\e[41m　　\e[40m　\e[41m　\e[40m　\e[41m　\e[40m　　　\e[43m　　　　　\e[40m　　\e[41m　\e[40m　　\e[41m　\e[40m　\e[41m　　\e[40m　\e[46m　\e[0m
+\e[46m　\e[40m　\e[41m　　\e[40m　\e[41m　　　\e[40m　　　\e[47m　　\e[41m　　\e[47m　\e[40m　　\e[41m　　\e[40m　\e[41m　\e[40m　\e[41m　\e[40m　\e[46m　　\e[0m
+\e[46m　　\e[40m　\e[41m　\e[40m　　　\e[41m　　\e[40m　　\e[45m　　　　　\e[40m　　\e[41m　　\e[40m　\e[41m　　\e[40m　\e[46m　　　\e[0m
+\e[46m　　　\e[40m　\e[46m　\e[40m　\e[47m　\e[40m　　　\e[47m　　\e[45m　　　　\e[40m　\e[41m　　\e[40m　\e[47m　\e[40m　　\e[46m　　　　\e[0m
+\e[46m　　　　　\e[40m　　\e[46m　\e[40m　　\e[45m　\e[47m　　\e[45m　　　\e[40m　　　\e[46m　\e[40m　　\e[46m　　　　　\e[0m
+\e[46m　　　　　　　\e[40m　　　\e[45m　　\e[47m　　\e[45m　　　\e[40m　　　\e[46m　　　　　　　\e[0m
+\e[46m　　　　　　　\e[40m　　　\e[45m　　　\e[47m　　\e[45m　　\e[40m　　　\e[46m　　　　　　　\e[0m
+\e[46m　　　　　　　　　\e[40m　\e[47m　\e[45m　　\e[40m　\e[47m　　　\e[40m　\e[46m　　　　　　　　　\e[0m
+\e[46m　　　　　　　　　　\e[40m　\e[41m　　\e[40m　\e[41m　　\e[40m　\e[46m　　　　　　　　　　\e[0m
+\e[46m　　　　　　　　　　　\e[40m　　\e[46m　\e[40m　　\e[46m　　　　　　2020.11.26\e[0m
+\e[46m　　　　　　　　　　　　　　　　　　　　　　　 atsushi\e[0m
+EOS
